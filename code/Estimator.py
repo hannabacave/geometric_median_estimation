e48211a9bab697b9631fbe60624994ed0b939471
hannabacave <hannabacave@gmail.com>
2020-11-02 16:10:46 +0100

    corps

---
+++ code/Estimator.py
@@ -0,0 +1,43 @@
+
+class Estimator
+
+    def __init__():
+
+
+    def median(self, d, data):
+        """ we want to get the argmin of the vector
+        accoridng to the following funciton:
+        ecrire fonction ici"""
+
+        median_result = argmin()
+        return median_result
+
+    def calculate_error_median(self, median_result, OTHER??): # TODO
+        """We calculate the prediction error from median"""
+
+    return median_error
+
+    def estimate(self, method):
+    """
+    Use estimator function to get
+    return: a dataframe with all the """
+    if (method = 1):
+    elif (method = 2):
+    elif (method = 3):
+    else:
+        estimated, estimated_error = estimator_function(data)
+        median_result = median(estimated) # permet de calculer la médiane grace à la fonciton créée
+        median_error = calculate_error_median(median_result, OTHER??)
+
+        compare_errors(median_error, estimation_error)
+
+    def compare_errors(self, median_error, estimation_error):
+
+
+
+
+def main():
+    data = # TODO recuperer données
+    e = Estimator(data)
+    result = e.estimate()
+    print(result)
